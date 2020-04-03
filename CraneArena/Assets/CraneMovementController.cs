5c110006af8467766a7b43e1e0fa773c2f18e3ca
KuKKilicious <clouseyda@yahoo.de>
2020-04-04 00:15:53 +0200

    Add Jump

--- CraneArena/Assets/CraneMovementController.cs
+++ CraneArena/Assets/CraneMovementController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class CraneMovementController : MonoBehaviour
 {
-    [SerializeField] GameObject m_BodyToRotate = null;
+    [SerializeField] GameObject m_TracksToRotate = null;
+    [SerializeField] GameObject m_CircleToRotate = null;
     [Header("Movement Properties")]
     [SerializeField] float m_MovementSpeed = 20f;
-    [SerializeField] float m_RotationSpeed = 40f;
+    [SerializeField] float m_TrackRotationSpeed = 40f;
+    [SerializeField] float m_CircleRotationSpeed = 30f;
     [SerializeField] bool useAngularVelocity = false;
+    [Header("Jump Properties")]
+    [SerializeField] float m_JumpForce= 20f;
     private Rigidbody m_Rigidbody = null;
 
     private float forwardValue = 0f;
-    private float rotationValue = 0f;
+    private float trackRotationValue = 0f;
+    private float circleRotationValue = 0f;
+
+    #region UNITY EVENTS
     // Start is called before the first frame update
     void Start()
     {
@@ -29,26 +36,50 @@ public class CraneMovementController : MonoBehaviour
     {
         Vector2 trueValue = value.Get<Vector2>();
         forwardValue = trueValue.y;
-        rotationValue = trueValue.x;
+        trackRotationValue = trueValue.x;
     }
 
-
-    private void FixedUpdate()
+    public void OnRotate(InputValue value)
     {
-        m_Rigidbody.velocity += m_BodyToRotate.transform.forward * forwardValue * m_MovementSpeed * Time.deltaTime;
+        float trueValue = value.Get<float>();
+        circleRotationValue = trueValue;
+    }
+    public void OnJump(InputValue value)
+    {
+        //check grounded 
+        //check used jump
 
+        m_Rigidbody.AddForce(Vector3.up * m_JumpForce);
+    }
+        private void FixedUpdate()
+    {
+        SetVelocity();
+        RotateBody(m_TracksToRotate, trackRotationValue, m_TrackRotationSpeed);
+        RotateBody(m_CircleToRotate, circleRotationValue, m_CircleRotationSpeed);
+    }
 
+    private void SetVelocity()
+    {
+        //Do ground Checks
+        m_Rigidbody.velocity += m_TracksToRotate.transform.forward * forwardValue * m_MovementSpeed * Time.deltaTime;
+    }
+
+    private void RotateBody(GameObject bodyToRotate,float rotationValue, float rotationspeed)
+    {
         if (useAngularVelocity)
         {
-            m_Rigidbody.angularVelocity += m_BodyToRotate.transform.up * rotationValue * m_RotationSpeed * Time.deltaTime;
-
+            m_Rigidbody.angularVelocity += bodyToRotate.transform.up * rotationValue * rotationspeed * Time.deltaTime;
         }
         else
         {
-            var toRotate = rotationValue * m_RotationSpeed * Time.deltaTime;
+            var toRotate = rotationValue * rotationspeed * Time.deltaTime;
 
-            m_BodyToRotate.transform.Rotate(new Vector3(0, toRotate, 0));
+            bodyToRotate.transform.Rotate(new Vector3(0, toRotate, 0));
         }
-
     }
+
+
+    #endregion
 }
+
+
