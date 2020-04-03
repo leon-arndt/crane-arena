083b6b92355e3eede574a628e238b6d78745ef79
Leon Arndt <neotalon27@gmail.com>
2020-04-03 18:39:12 +0200

    Add git ignore and singleton

---
+++ Unity/Assets/Scripts/MonoBehaviourSingleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+
+                if (_instance == null)
+                {
+                    throw new InvalidOperationException($"No {nameof(T)} in current scene");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = gameObject.GetComponent<T>();
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _instance = null;
+    }
+}
