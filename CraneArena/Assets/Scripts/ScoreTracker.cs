aedc141779b52ea5c8dddc2a2247d26ac0d69c21
Leon Arndt <neotalon27@gmail.com>
2020-04-04 18:44:30 +0200

    Extend score tracker to four players

--- CraneArena/Assets/Scripts/ScoreTracker.cs
+++ CraneArena/Assets/Scripts/ScoreTracker.cs
@@ -9,20 +9,30 @@ using UnityEngine;
 public class ScoreTracker : MonoBehaviourSingleton<ScoreTracker>
 {
     [SerializeField]
-    private int m_player1Score, m_player2Score;
+    private int m_player1Score, m_player2Score, m_player3Score, m_player4Score;
 
     // Start is called before the first frame update
     void Start()
     {
+        ResetScores();
+    }
+
+    /// <summary>
+    /// Reset all player scores to zero
+    /// </summary>
+    private void ResetScores()
+    {
         m_player1Score = 0;
         m_player2Score = 0;
+        m_player3Score = 0;
+        m_player4Score = 0;
     }
 
     /// <summary>
     /// Increase the desired player score by one
     /// Could already be refactored into two individual playerScore classes with fields
     /// </summary>
-    /// <param name="playerId"></param>
+    /// <param name="playerId">The index of the player whose scores should be increased</param>
     public static void IncreaseScoreByOne(int playerId)
     {
         if (playerId == 0)
@@ -34,7 +44,16 @@ public class ScoreTracker : MonoBehaviourSingleton<ScoreTracker>
         {
             Instance.m_player2Score++;
             ScoreUi.UpdateScore(playerId, Instance.m_player2Score);
-
+        }
+        else if (playerId == 2)
+        {
+            Instance.m_player3Score++;
+            ScoreUi.UpdateScore(playerId, Instance.m_player3Score);
+        }
+        else if (playerId == 3)
+        {
+            Instance.m_player4Score++;
+            ScoreUi.UpdateScore(playerId, Instance.m_player4Score);
         }
         else
         {
