99cad21b6af35783588f2be1c0a466a33e5df650
Leon Arndt <neotalon27@gmail.com>
2020-04-04 22:44:12 +0200

    Add hazard spawner

---
+++ CraneArena/Assets/Scripts/HazardSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spawn rigidbody hazards over time
+/// </summary>
+public class HazardSpawner : MonoBehaviourSingleton<HazardSpawner>
+{
+    [SerializeField]
+    private GameObject[] hazardPrefabs;
+
+    private int index;
+
+    public void SpawnHazard()
+    {
+        int index = Random.Range(0, hazardPrefabs.Length);
+        GameObject hazard = Instantiate(hazardPrefabs[index]);
+
+        //update hazard position
+        hazard.transform.position = new Vector3(0, 20, 0);
+    }
+
+    IEnumerator SpawnHazardRoutine()
+    {
+        if (GameManager.Instance.timeLeft > 10f)
+        {
+            SpawnHazard();
+        }
+        yield return new WaitForSeconds(10f); 
+    }
+}
