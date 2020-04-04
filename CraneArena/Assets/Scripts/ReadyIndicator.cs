67d56b948ac556e8d66cc61fbedc01b9da7f856c
Leon Arndt <neotalon27@gmail.com>
2020-04-04 18:39:13 +0200

    Start work on ready indicator

---
+++ CraneArena/Assets/Scripts/ReadyIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scene object which indicates that the player is ready
+/// </summary>
+public class ReadyIndicator : MonoBehaviour
+{
+    public static List<ReadyIndicator> indicators;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        indicators.Add(this);
+    }
+
+    public static void DestroyAll()
+    {
+        foreach (var item in indicators)
+        {
+            Destroy(item.gameObject);
+        }
+    }
+}
