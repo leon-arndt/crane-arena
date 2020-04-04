7300d3bd36e1e0269b8ad0277090b603f67d33b8
Leon Arndt <neotalon27@gmail.com>
2020-04-04 23:02:53 +0200

    Freeze RB temporarily after colliding to avoid janky physics and bouncing objects

---
+++ CraneArena/Assets/Scripts/RemoveConstraintsOnImpact.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Remove constrains on rigidbody on impact
+/// </summary>
+public class RemoveConstraintsOnImpact : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Invoke("RemoveConstraints", 1f);
+    }
+
+    public void RemoveConstraints()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+}
