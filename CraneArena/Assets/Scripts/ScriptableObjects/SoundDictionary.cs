1fe7dd8cc5eaa66da62ba00d0fc2d65daa9785e7
Leon Arndt <neotalon27@gmail.com>
2020-04-04 19:00:42 +0200

    Start work on audio structure

---
+++ CraneArena/Assets/Scripts/ScriptableObjects/SoundDictionary.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundDictionary", menuName = "ScriptableObjects/SoundDictionary", order = 1)]
+public class SoundDictionary : ScriptableObject
+{
+    public SoundEvent[] dictionary;
+}
---
+++ CraneArena/Assets/Scripts/ScriptableObjects/SoundEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "SoundEvent", menuName = "ScriptableObjects/SoundEvent", order = 1)]
+public class SoundEvent : ScriptableObject
+{
+    //Make sure that the eventName is only updated automatically
+    [ReadOnly]
+    public string eventName;
+
+    public AudioClip clip;
+
+    [Range(0, 1)]
+    public float volume;
+
+   public void OnValidate()
+    {
+        FileNameToNameField();
+    }
+
+    /// <summary>
+    /// Automatically update the eventName to the fileName to avoid errors and naming effor
+    /// </summary>
+    public void FileNameToNameField()
+    {
+        string assetPath = AssetDatabase.GetAssetPath(this.GetInstanceID());
+        eventName = Path.GetFileNameWithoutExtension(assetPath);
+    }
+}
