426066deae434ba6023aee3f3157740a83251cd9
Leon Arndt <neotalon27@gmail.com>
2020-04-05 10:16:56 +0200

    Missing scripts

---
+++ CraneArena/Assets/Scripts/WinnerScreenUi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Sirenix.OdinInspector;
+/// <summary>
+/// Show UI information for the winner at the end of each round
+/// </summary>
+public class WinnerScreenUi : MonoBehaviourSingleton<WinnerScreenUi>
+{
+    public TextMeshProUGUI winnerText;
+
+    [Button]
+    public static void UpdateWinner(int playerId)
+    {
+        string winString = "Player " + playerId.ToString() + " wins the round!";
+        Instance.winnerText.text = winString;
+    }
+}
